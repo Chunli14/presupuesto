b73a78e58c1d494f124b96eecf5500e7c315cc84
Nohemi <nohemigomez4322@gmail.com>
2022-07-28 09:24:27 -0400

    actualizacion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Pregunta from './components/Pregunta';
 import Formulario from './components/Formulario';
 import Listado from './components/Listado';
@@ -10,14 +10,22 @@ function App() {
   const [ restante, guardarRestante ] = useState(0);
   const [ mostrarpregunta, actualizarPregunta] = useState(true);
   const [ gastos, guardarGastos ] = useState([]);
+  const [ gasto, guardarGasto ] = useState({});
+  const [ creargasto, guardarCrearGasto ] = useState(false);
 
-  const agregarNuevoGasto = gasto => {
-    guardarGastos([
-      ...gastos,
-      gasto
-    ])
+  useEffect(() => {
+    if(creargasto) {
+      guardarGastos([
+        ...gastos,
+        gasto
+      ]);
+      
+      const presupuestoRestante = restante - gasto.cantidad
+      guardarRestante(presupuestoRestante);
+      guardarCrearGasto(false);
+    }
+  }, [gasto, creargasto, gastos, restante]);
 
-  }
 
   return (
     <div className='container'>
@@ -37,13 +45,14 @@ function App() {
            <div className='row'>
              <div className='one-half column'>
                 <Formulario
-                agregarNuevoGasto={agregarNuevoGasto}
+                guardarGasto={guardarGasto}
+                guardarCrearGasto={guardarCrearGasto}
                 />
              </div>
 
              <div className='one-half column'>
               <Listado 
-              gasto={gastos} 
+                gastos={gastos} 
               />
 
               <ControlPresupuesto 
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import Error from "./error";
+import PropTypes from 'prop-types';
 import shortid from 'shortid';
-const Formulario = ({agregarNuevoGasto}) => {
+import Error from "./error";
+const Formulario = ({guardarGasto, guardarCrearGasto}) => {
 
     const [ nombre, guardarNombre ] = useState('');
     const [ cantidad, guardarCantidad ] = useState(0);
@@ -22,7 +23,8 @@ const Formulario = ({agregarNuevoGasto}) => {
             id: shortid.generate()
         }
  
-        agregarNuevoGasto(gasto);
+        guardarGasto(gasto);
+        guardarCrearGasto(true);
         guardarNombre('');
         guardarCantidad(0);
 
@@ -41,7 +43,7 @@ const Formulario = ({agregarNuevoGasto}) => {
                    className="u-full-width"
                    placeholder="Ej. Transporte"
                    value={nombre}
-                   oneChange={e => guardarNombre(e.target.value)}
+                   onChange={e => guardarNombre(e.target.value)}
                 />
             </div>
 
@@ -52,7 +54,7 @@ const Formulario = ({agregarNuevoGasto}) => {
                    className="u-full-width"
                    placeholder="Ej. 300"
                    value={cantidad}
-                   oneChange={e => guardarCantidad(parseInt(e.target.value, 10))}
+                   onChange={e => guardarCantidad(parseInt(e.target.value, 10))}
                 />
             </div>
 
@@ -65,4 +67,9 @@ const Formulario = ({agregarNuevoGasto}) => {
      );
 }
  
+Formulario.propTypes = {
+    guardarGasto: PropTypes.func.isRequired,
+    guardarCrearGasto: PropTypes.func.isRequired
+}
+
 export default Formulario;
